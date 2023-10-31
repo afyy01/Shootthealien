05bb1ac0d23fc6c7b1eaef70d912436cb1cf0845
afyy01 <103791068+afyy01@users.noreply.github.com>
2023-10-31 00:51:17 +0000

    Add files via upload

---
+++ alien.py
@@ -0,0 +1,30 @@
+import pgzrun
+from random import randint
+#specification for pgzero screen
+TITLE = "Alien shooting game"
+WIDTH = 1000
+HEIGHT = 600
+
+message = ""
+
+alien = Actor("alien")
+
+def draw():
+    screen.clear()
+    screen.fill(color=(0,0,185))
+    alien.draw()
+    screen.draw.text(message,center=(500,40), fontsize=60)
+
+def place_alien():
+    alien.x = randint(50, WIDTH -50)
+    alien.y = randint(50,HEIGHT -50)
+
+def on_mouse_down(pos):
+    global message
+    if alien.collidepoint(pos):
+        message = "Good Shot!"
+        place_alien()
+    else:
+        message = "You missed!"
+place_alien()
+pgzrun.go()
